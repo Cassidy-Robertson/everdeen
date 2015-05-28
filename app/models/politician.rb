1d9cf28ed4e4bc4ba55d709cf121f9820a54a59f
Cassidy-Robertson <Cassidydrobertson@gmail.com>
2015-05-28 10:26:12 -0400

    used the twitter api, trying to get mentions to show on the twitter widget

--- app/models/politician.rb
+++ app/models/politician.rb
@@ -1,2 +1,34 @@
 class Politician < ActiveRecord::Base
+  def mentions_timeline(topics)
+    # topics = ["@politician.twitter_handle"]
+    @topics_twitter = twitter_client.filter(track: topics.join(",")) do |object|
+      puts object.text if object.is_a?(Twitter::Tweet)
+    end
+  end
+
+
+  def twitter_client
+    Twitter::Streaming::Client.new do |config|
+      config.consumer_key        = "LvxXJLXdqzg6OpBq5u2JYdkpT"
+      config.consumer_secret     = "MpzOhlewztGAdRqdE1hkhX3RQ9qNmfhMMo24HMlZt7qOYydsz9"
+      config.access_token        = "568811961-bjaGtWBu4tDlRtpP4ldj2uEa0IcEgLu3PFtbtPXz"
+      config.access_token_secret = "Xhvpc6XxGS3NO4kJQHiT1pwHuzavj6PjlCNUgEafTJLe0"
+    end
+  end
+
+  # def capitalize_string(string)
+  #   string.capitalize
+  # end
+
 end
+
+# Politician.mentions_timeline("coffee")
+# ^^ WRONG
+# kenny = Politician.new
+
+# kenny.mentions_timeline("politics")
+
+
+# your_name = "cassidy"
+# your_name.capitalize
+# capitalize
